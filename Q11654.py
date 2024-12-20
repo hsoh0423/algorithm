f94aa36783b8486e458fd4064db0aedcb2cc6868
hsoh0423 <hsoh0423@naver.com>
2024-12-20 15:51:43 +0900

    2024/12/19

---
+++ Q11654.py
@@ -0,0 +1,2 @@
+input_a = input()
+print(ord(input_a))
---
+++ Q25083.py
@@ -0,0 +1,8 @@
+print("         ,r'", end="")
+print('"7')
+print("r`-_   ,'  ,/")
+print(' \. ". L_r', end='')
+print("'")
+print("   `~\/")
+print("      |")
+print("      |")
