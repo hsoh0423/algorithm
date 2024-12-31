5d7797303d72415e02f26e9a4814ea712d4591ef
hsoh0423 <hsoh0423@naver.com>
2024-12-31 17:02:16 +0900

    2024-12-31

--- Q1259.py
+++ Q1259.py
@@ -1,4 +1,21 @@
-while(True):
-    input_num = input()
+def is_palindrome(num):
     
-    if
+    temp_num = str(num)
+    answer = True
+    for i in range(int(len(temp_num)/2)):
+        if temp_num[i] != temp_num[(len(temp_num) - 1) - i]:
+            answer = False
+    
+    return answer
+
+while(True):
+    input_num = int(input())
+    
+    if input_num == 0:
+        break
+    else:
+            
+        if is_palindrome(input_num):
+            print("yes")
+        else:
+            print("no")
